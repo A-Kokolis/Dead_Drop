678cd2901997548cd95379669bcbc6b170796fd9
A-Kokolis <apostolis_kok@hotmail.com>
2017-10-02 00:10:24 -0500

    receiver communication side

--- sender.cpp
+++ sender.cpp
@@ -6,6 +6,26 @@
 #define set_jump 1024
 #define repetition_num 100
 
+
+void establish_covert_channel(char *communication_buffer){
+ int i,iter;
+
+ //Write to specific sets in the LLC cache to figure out the cache sets that we are
+ //gonna be using for the communication
+ for(i=0; i<set_number; i+= set_jump){
+
+  for(iter=0; iter<repetition_num; iter++){
+	communication_buffer[i] = 'a';	
+  }
+}
+
+
+}
+
+
+
+
+
 int main(int argc, char **argv)
 {
 	// Put your covert channel setup code here
@@ -15,15 +35,6 @@ int main(int argc, char **argv)
 	char *communication_buffer;
 
 	communication_buffer = (char*)malloc(sizeof(char)*8000000);	
-
-	//Write to specific sets in the LLC cache to figure out the cache sets that we are
-	//gonna be using for the communication
-	for(i=0; i<set_number; i+= set_jump){
-
-	  for(iter=0; iter<repetition_num; iter++){
-		communication_buffer[i+j] = 'a';	
-	  }
-	}
 
 	printf("Please type a message.\n");
 
